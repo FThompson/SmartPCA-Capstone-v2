c80a72ba5d4fb04d9b5451c2e0475f19ac6d1107
Finn Thompson <nnifinn@gmail.com>
2018-05-17 18:29:34 -0700

    Added Component and ComponentHandler. Updated Device logic. Fixed upload.sh

--- src/device.py
+++ src/device.py
@@ -1,28 +1,63 @@
 import os
 import pigpio
 import pygame
+from states import State
+from physical.servo import Servo
+from physical.stepper import Stepper
 from ui.colors import Color
+from ui.component import ComponentHandler
 
 FRAME_BUFFER = '/dev/fb1'
 SCREEN_SIZE = (480, 320)
+EVENT_TYPES = (pygame.MOUSEBUTTONDOWN, pygame.MOUSEBUTTONUP, pygame.QUIT)
+
+SERVO_PIN = 25
+STEPPER_PINS = (4, 17, 23, 24)
 
 class Device:
     def __init__(self):
+        print('Initializing pigpio... ')
         self.gpio = pigpio.pi()
-        os.putenv('SDL_FBDEV', FRAME_BUFFER)
-        pygame.init()
-        self.lcd = pygame.display.set_mode(SCREEN_SIZE)
-        self.lcd.fill(Color.RIIT_BLUE.value)
-        pygame.mouse.set_visible(False)
-        pygame.display.update()
+        print('Done.')
+        print('Initializing pygame... ')
+        self.setup_pygame()
+        print('Done.')
+        print('Initializing components... ')
+        self.state = State.HOME
+        self.servo = Servo(self.gpio, SERVO_PIN)
+        self.stepper = Stepper(self.gpio, 512, *STEPPER_PINS)
+        self.component_handler = ComponentHandler()
+        self.running = True
+        print('Done.')
 
     def __del__(self):
         self.gpio.stop()
 
+    def setup_pygame(self):
+        os.putenv('SDL_FBDEV', FRAME_BUFFER)
+        pygame.init()
+        #pygame.event.set_allowed(EVENT_TYPES)
+        self.screen = pygame.display.set_mode(SCREEN_SIZE)
+        self.screen.fill(Color.RIIT_BLUE.value)
+        pygame.mouse.set_visible(False)
+        pygame.display.update()
+
+    def run(self):
+        print('Running.\n')
+        while self.running:
+            self.update()
+
     def update(self):
+        if pygame.event.peek(pygame.QUIT):
+            self.running = False
+        else:
+            self.servo.update()
+            self.stepper.update()
+            self.component_handler.update(self.state)
+
+    def set_backlight(self, brightness):
         pass
 
 if __name__ == '__main__':
     device = Device()
-    while True:
-        device.update()
+    device.run()
---
+++ src/ui/component.py
@@ -0,0 +1,56 @@
+import pygame
+from abc import ABC, abstractmethod
+
+class Component(ABC):
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    @abstractmethod
+    def get_valid_states(self):
+        pass
+
+    @abstractmethod
+    def on_paint(self):
+        pass
+
+    @abstractmethod
+    def on_press(self):
+        pass
+
+    @abstractmethod
+    def on_click(self):
+        pass
+
+    def contains(self, x, y):
+        return x >= self.x and x <= self.x + self.w and y >= self.y and y <= self.y + self.h
+
+    def dx(self, x):
+        return x + self.x
+
+    def dy(self, y):
+        return y + self.y
+
+class ComponentHandler():
+    def __init__(self):
+        self.components = []
+
+    def add(self, component):
+        self.components.append(component)
+
+    def update(self, state):
+        event_queue = pygame.event.get()
+        mouse_events = [e for e in event_queue if e.type != pygame.QUIT]
+        for e in mouse_events:
+            print('mouse event type {} at {},{}'.format(e.type, e.pos.x, e.pos.y))
+        for component in self.components:
+            if state in component.get_valid_states():
+                component.paint()
+                for event in mouse_events:
+                    if component.contains(event.pos.x, event.pos.y):
+                        if event.type == pygame.MOUSEBUTTONDOWN:
+                            component.on_press()
+                        elif event.type == pygame.MOUSEBUTTONUP:
+                            component.on_click()
